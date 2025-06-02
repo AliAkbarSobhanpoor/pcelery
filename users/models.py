0440f89e1cb8f5e09f32a2cca0af362ae324a524
AliAkbarSobhanpour <a.a.s.javid0416@gmail.com>
2025-06-02 23:12:29 +0330

    add send message config and also . config the first part of the celery .

--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
 # Create your models here.
 
 
@@ -7,5 +7,4 @@ class User(AbstractUser):
     """
     custom user . each website may have its functionality and field required for this part so we do this
     """
-
-    pass
+    must_change_password = models.BooleanField(default=False)
